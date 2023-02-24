7ed4b920122918ca580ef049a008ea5947087d96
João Henrik <jh.signori@gmail.com>
2023-02-24 20:28:19 -0300

    Consuming Sweet Alert 2.

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,11 @@
+import Swal from 'sweetalert2'
+import withReactContent from 'sweetalert2-react-content'
 import { coffees } from '../../data/Coffees'
 import { Slogan } from './components/Slogan'
 import { CoffeeCard } from './components/CoffeeCard'
 import { CoffeesContainer, CoffeesList } from './styles'
+
+export const MySwal = withReactContent(Swal)
 
 export function Home() {
   return (
