9fcd4285b80ffbd8661771c667953493b57bf0d8
João Henrik <jh.signori@gmail.com>
2023-02-24 17:17:39 -0300

    Updated the style of payment method entries.

--- src/pages/Checkout/components/CompleteOrder/components/PaymentMethods/styles.ts
+++ src/pages/Checkout/components/CompleteOrder/components/PaymentMethods/styles.ts
@@ -36,12 +36,14 @@ export const FormOfPaymentContainer = styled.div`
   gap: 1.2rem;
 `
 
-export interface PaymentMethodButtonProps {
-  selectedButton: string
+export interface PaymentMethodInputProps {
+  customInput: string
 }
 
-export const PaymentMethodButton = styled.button<PaymentMethodButtonProps>`
+export const PaymentMethodInput = styled.div<PaymentMethodInputProps>`
   font-size: 1.2rem;
+
+  position: relative;
 
   width: 17rem;
   padding: 1.6rem;
@@ -49,18 +51,33 @@ export const PaymentMethodButton = styled.button<PaymentMethodButtonProps>`
   border-radius: 6px;
   border: 1px solid ${(props) => props.theme['base-button']};
   background-color: ${(props) => props.theme['base-button']};
+  overflow: hidden;
 
-  display: flex;
-  align-items: center;
-  justify-content: left;
-  gap: 1.2rem;
+  label {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 1.2rem;
 
-  svg {
-    color: ${(props) => props.theme.purple};
+    svg {
+      color: ${(props) => props.theme.purple};
+    }
   }
 
-  &.${(props) => props.selectedButton} {
-    border: 1px solid ${(props) => props.theme.purple};
+  &.${(props) => props.customInput} {
     background-color: ${(props) => props.theme['purple-light']};
+    border: 1px solid ${(props) => props.theme.purple};
+  }
+
+  input[type='radio'] {
+    position: absolute;
+    z-index: 2;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+
+    -webkit-appearance: none;
+    cursor: pointer;
   }
 `
