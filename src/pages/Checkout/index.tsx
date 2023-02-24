1342f58877468956c6652978df757b404f6913a7
João Henrik <jh.signori@gmail.com>
2023-02-24 19:02:39 -0300

    Integrating ZOD in the react hook form, created and incremented a validation scheme.

--- src/pages/Checkout/index.tsx
+++ src/pages/Checkout/index.tsx
@@ -1,18 +1,47 @@
+/* eslint-disable no-unused-vars */
 import { FormProvider, useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import * as zod from 'zod'
 import { useNavigate } from 'react-router-dom'
 import { Cart } from './components/Cart'
 import { CompleteOrder } from './components/CompleteOrder'
 import { CheckoutContainerForm } from './styles'
 
+const PAYMENT_VALUES = ['credit', 'debt', 'money'] as const
+
+const confirmeOrderValidationSchema = zod.object({
+  cep: zod.string().min(1, 'Informe o CEP'),
+  road: zod.string().min(1, 'Informe a Rua'),
+  number: zod.string().min(1, 'Informe o número'),
+  complement: zod.string().min(1, 'Informe o complemento'),
+  district: zod.string().min(1, 'Informe o Bairro'),
+  city: zod.string().min(1, 'Informe a Cidade'),
+  state: zod.string().min(1, 'Informe o Estado'),
+  selectedPaymentMethod: zod.enum(PAYMENT_VALUES),
+})
+
+export type ConfirmeOrderData = zod.infer<typeof confirmeOrderValidationSchema>
+
 export function Checkout() {
-  const confirmeOrderForm = useForm()
+  const confirmeOrderForm = useForm<ConfirmeOrderData>({
+    resolver: zodResolver(confirmeOrderValidationSchema),
+    defaultValues: {
+      cep: '',
+      road: '',
+      number: '',
+      complement: '',
+      district: '',
+      city: '',
+      state: '',
+      selectedPaymentMethod: 'credit',
+    },
+  })
+
   const { handleSubmit } = confirmeOrderForm
 
   const navigate = useNavigate()
 
-  function handleConfirmOrder(data: any) {
-    console.log(data)
-    console.log('Pedido confirmado')
+  function handleConfirmOrder(data: ConfirmeOrderData) {
     navigate('/success', {
       state: data,
     })
