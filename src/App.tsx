19b9bf4c4f7527986c9f2ec6143edefa7b42dc7f
João Henrik <jh.signori@gmail.com>
2023-02-23 23:56:09 -0300

    Inserted the payment methods context provider.

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/default'
 import { CartContextProvider } from './contexts/CartContext'
+import { PaymentMethodProvider } from './contexts/PaymentMethod'
 import { Router } from './Router'
 import { GlobalStyle } from './styles/global'
 
@@ -10,8 +11,10 @@ export function App() {
     <ThemeProvider theme={defaultTheme}>
       <BrowserRouter>
         <CartContextProvider>
-          <Router />
-          <GlobalStyle />
+          <PaymentMethodProvider>
+            <Router />
+            <GlobalStyle />
+          </PaymentMethodProvider>
         </CartContextProvider>
       </BrowserRouter>
     </ThemeProvider>
